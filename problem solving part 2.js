17f9e7f8ce9fda4d6c11b8d6c003d9a7adf96d8c
Ebony Riddick <ebonyriddick@gmail.com>
2021-09-28 16:56:16 -0400

    pasted information removed from PS worksheet document and pasted into PS worksheet 2 document.

---
+++ problem solving part 2.js
@@ -0,0 +1,29 @@
+// count from numbers 1 to 100
+// check to see if a number is divisible by only 1 and itself
+// NOTE: cannot be divisible by any other number
+// ex. 10 - only divisible by 1 and 10, not 2,3,4,5,6,7,8,9
+// in order to check if 10 is prime, we also need to divide it by all numbers that came before it
+// IF a number is prime, print it to the console!
+
+// let counter = 1
+
+
+
+// for(let i = 0; i < 101; i++){
+//     //I want to dive i by itself 
+//     if(counter % 1 === 0 && counter % i === 0){
+//         console.log(i);
+//     }
+//     // if(i /= i){
+//     //     console.log(i);
+//     // }   
+//     // if(i % i);{
+//     //     console.log(i);
+//     // }
+//     // if(i %= i);{
+//     //     console.log(i);
+//     // }
+    
+    
+
+// }
